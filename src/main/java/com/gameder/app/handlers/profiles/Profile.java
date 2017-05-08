27b81df942dee78befc16980400d22ba8cbbf6fd
willlyprkl <vili.korhonen@hotmail.fi>
2017-05-08 13:37:45 +0300

    Treeset implemented along with many other great features

--- src/main/java/com/gameder/app/handlers/profiles/Profile.java
+++ src/main/java/com/gameder/app/handlers/profiles/Profile.java
@@ -1,12 +1,14 @@
 package com.gameder.app.handlers.profiles;
 
-public class Profile {
+public class Profile implements Comparable<Profile>{
     private final String name;
     private final String img;
+    private int preference;
 
-    public Profile(String name, String img) {
+    public Profile(String name, String img, int preference) {
         this.name = name;
         this.img = img;
+        this.preference = preference;
     }
 
     public String getName() {
@@ -16,4 +18,25 @@ public class Profile {
     public String getImg() {
         return this.img;
     }
+
+    public int getPreference() {
+        return this.preference;
+    }
+
+    @Override
+    public int compareTo(Profile p) {
+        if(this.getPreference() < p.getPreference()) {
+            return  -1;
+        } else if (this.getPreference() > p.getPreference()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " , " + this.img + " , " + this.preference;
+    }
+
 }
